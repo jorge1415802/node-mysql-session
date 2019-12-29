0a09e5583f5355063579bf7c606a9da6c0142314
Jorge <rjogeluis64@gmail.com>
2019-12-29 18:18:04 -0300

    version 3

--- src/routes/enlaces.js
+++ src/routes/enlaces.js
@@ -9,6 +9,12 @@ router.get('/agregar',(req,res) => {
 router.post('/agregar',async(req,res) => {
   console.log(req.body);
   await db.query('INSERT INTO Enlaces set ?',[req.body]);
-  res.send('Datos recibidos');
+  res.redirect('/enlaces');
+});
+
+router.get('/',async(req,res) => {
+  var enlaces = await db.query('SELECT * FROM Enlaces');
+  console.log(enlaces);
+  res.render('enlaces/lista',{enlaces});
 });
 module.exports = router;
